23f23ca6d7e0f5ef1a5a53f571c2cdd1d826218c
xiling zhang <55151938+xiling42@users.noreply.github.com>
2020-11-21 16:28:06 -0500

    Update util.py

--- LDFI/util.py
+++ LDFI/util.py
@@ -81,7 +81,7 @@ def _get_request_by_type(request_type, firt_run):
     #python_command = 'jmeter.bat -n -t C:\Users\Ling\OneDrive\Documents\Brown-DESKTOP-8B9G99R\ds-microservices\final-project\jmeter-data\jmeter_tests\type_admin_get_orders.jmx  -l C:\Users\Ling\OneDrive\Documents\Brown-DESKTOP-8B9G99R\ds-microservices\final-project\jmeter-data\jmeter_tests\logs\test1.txt'
     # run_command
     os.popen(command)
-    time.sleep(2)
+    time.sleep(5)
     success = _get_result_from_log(request_log)
     
     if not success:
@@ -125,7 +125,9 @@ def _extract_services_set(j, bfile = False):
         with open(j) as f:
             data = json.load(f)['data']
     else:
-        data = json.load(j)['data']
+        data = json.load(j)
+        print(data)
+        data = data['data']
         
     result = list()
     for trace in data:
