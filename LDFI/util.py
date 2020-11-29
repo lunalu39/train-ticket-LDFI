0e68aa73004705ae5cdcc66e415556b31a2036f0
lunalu39 <lunalu9739@gmail.com>
2020-11-29 15:03:06 -0500

    update

--- LDFI/util.py
+++ LDFI/util.py
@@ -13,10 +13,12 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import yaml
 
-requests = ['type_admin_get_orders', 'type_simple_search', 'type_admin_get_route', 
-            'type_admin_get_travel', 'type_admin_login',
-            'type_cheapest_search', 'type_food_service', 'type_preserve',
-            'type_user_login']
+# requests = ['type_admin_get_orders', 'type_simple_search', 'type_admin_get_route', 
+#             'type_admin_get_travel', 'type_admin_login',
+#             'type_cheapest_search', 'type_food_service', 'type_preserve',
+#             'type_user_login']
+requests = ['type_admin_get_route', 'type_food_service', 'type_simple_search', 'type_admin_get_orders', 'type_admin_get_travel', 'type_admin_login',
+                'type_cheapest_search',  'type_preserve', 'type_user_login']
 #requests = ['type_admin_get_orders']
 
 # ts-security-service.default.svc.cluster.local:11188/*
