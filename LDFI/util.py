91e87f5553ceea24f572e398145a8d936f3a42a9
lunalu39 <lunalu9739@gmail.com>
2020-12-06 11:33:05 -0500

    format

--- LDFI/util.py
+++ LDFI/util.py
@@ -3,7 +3,7 @@
 This file contains utility functinos for LDFI. Including fault injection and recovery, Jaeger traces extraction and retrival.
 Created on Sat Nov 21 00:38:07 2020
 
-@author: Ling
+@author: Xiling Zhang, Qingyi Lu
 """
 import os
 import time
@@ -79,6 +79,7 @@ def inject_and_get_trace(list_service, fault, request_type):
         proc = subprocess.Popen(command, shell=True,  stdout=subprocess.PIPE)
         
     return request_result
+  
 def get_request_type_traces(targeted_requests = requests):
     # for given requests, run them and get all traces in dictionary. Requests are default to be all request types.
     
@@ -99,7 +100,6 @@ def get_request_type_traces(targeted_requests = requests):
         #     traces[request] = services
     return traces
 
-
 def _write_yaml(service_name, fault_type):
     # create a yaml file for given service and fault type. 
     print('debug: ', service_name)
@@ -110,7 +110,6 @@ def _write_yaml(service_name, fault_type):
     else:
         print('error in fault type. Please check!!!!')
         return
-    
     
     with open(template) as f:
         content = yaml.load(f, Loader = yaml.FullLoader)
@@ -128,8 +127,6 @@ def _write_yaml(service_name, fault_type):
     with open(service_name + '-' + fault_type + '.yml', 'w') as nf:
         yaml.dump(content, nf)
         
-    
-
 def _get_result_from_log(file_path):
     # get result for file
     success_index = 7
@@ -144,8 +141,6 @@ def _get_result_from_log(file_path):
 
     return success
 
-
-
 def _get_request_by_type(request_type, firt_run):
     # for given type, run request and get traces. 
     # if first_run, there should be no error. Throw exception and terminate if there is any. This suggest Train Ticket works unexpected.
@@ -158,11 +153,9 @@ def _get_request_by_type(request_type, firt_run):
     else:
         jmeter_exec = os.path.join(jmeter_path, 'jmeter.bat')
     
-    
     if firt_run:
         if os.path.exists(request_log):
             os.remove(request_log) # if there is no error, remove log
-            
             
     command = '{} -n -t {} -l {}'.format(jmeter_exec, request_file, request_log)
     #python_command = 'jmeter.bat -n -t 
@@ -205,11 +198,9 @@ def _get_trace_from_jaeger(request_type):
     json_data, error = proc.communicate()
     return _extrace_services_set_basedon_operation(request_type, json_data, False)
          
-    
 def _get_milliseconds_time(date_time):
     # change time to Jaeger api required time
     return int(date_time.timestamp() * 1000000)
-
 
 def _extrace_services_set_basedon_operation(request_type, j, bfile = False): 
     # return list of services
@@ -250,8 +241,6 @@ def _extrace_services_set_basedon_operation(request_type, j, bfile = False):
             services_set.add(processes[service]['serviceName'])
         result = list(services_set)
     return result
-
-
 
 #traces = get_request_type_traces()
 #j = json.dumps(traces)
