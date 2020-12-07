c9ea467be3096f243f725b25d6dbd2ab6f5c21d3
xiling zhang <55151938+xiling42@users.noreply.github.com>
2020-12-06 21:33:03 -0500

    Update random_inject.py

--- LDFI/random_inject.py
+++ LDFI/random_inject.py
@@ -9,6 +9,7 @@ For all services, system starts to random from combinnation length r = 1
 @author: Ling
 """
 from util import get_request_type_traces, inject_and_get_error_requests
+import random
 
 from itertools import combinations
 requests = ['type_admin_get_route', 'type_food_service', 'type_simple_search', 'type_admin_get_orders', 'type_admin_get_travel', 'type_admin_login',
@@ -35,23 +36,36 @@ def main():
     # init run get requests traces 
     get_request_type_traces()
     counter = 0
-    r = 1
-    comb = combinations(microservices, r)
+    r = 0
+    comb = []
+    p = 0
     while len(requests) > 0:        
-        # random get a combination
-        try:
-            services = next(comb)
-        except StopIteration:
-            print('combination for length ', r, ' ends. +1..')
+        # random get one from combination
+        if p == len(comb):
             r += 1
+            
             if r > len(microservices):
                 print('reach all combinations, cannot finish....')
-            comb = combinations(microservices, r)
-            continue
+                break
+            # get combination and random. generator random? 
+            comb = list(combinations(microservices, r))
+            random.shuffle(comb)
+            p = 0
+            
+        services = comb[p]
+        p += 1
+        # try:
+        #     services = next(comb)
+        # except StopIteration:
+        #     print('combination for length ', r, ' ends. +1..')
+        #     r += 1
+        #     if r > len(microservices):
+        #         print('reach all combinations, cannot finish....')
+        #     comb = combinations(microservices, r)
+        #     continue
  
         # inject failures
         # get request types
-
         errored_services = inject_and_get_error_requests(services, 'delay')
         errored_services = inject_and_get_error_requests(services, 'abort')
         
